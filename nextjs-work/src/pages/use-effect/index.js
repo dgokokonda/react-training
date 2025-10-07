4152d3d0fba56d3dafe6562e3737a30344b6b3aa
dgokokonda <sunny-bvb@mail.ru>
2025-10-07 11:48:08 +0300

    rename files and folders by rules

--- nextjs-work/src/pages/use-effect/index.js
+++ nextjs-work/src/pages/use-effect/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import "./Timer.css";
+// import "./timer.css";
 
 function CountdownTimer() {
   const [timeLeft, setTimeLeft] = useState(60); // 60 секунд
